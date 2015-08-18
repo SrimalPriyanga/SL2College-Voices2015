b0a5367266a3f02b9e25aed0002280f5f7a8bcb9
Srimal Priyanga <s.priyanga22@gmail.com>
2015-08-18 23:35:00 +0530

    countdown date changes & style changes

--- www/js/script.js
+++ www/js/script.js
@@ -1,13 +1,13 @@
 (function($){
 	
 	var note = $('#note'),
-		ts = new Date(2015, 08, 19),
-		newYear = false;
+		ts = new Date(2015, 08, 19, 08, 30),
+		newYear = true;
 	
 	if((new Date()) > ts){
 		// The new year is here! Count towards something else.
 		// Notice the *1000 at the end - time must be in milliseconds
-		ts = (new Date()).getTime() + 10*24*60*60*1000;
+		ts = (new Date()).getTime();
 		newYear = false;
 	}
 		
